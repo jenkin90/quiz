a15aca06318139632dcd4023d764821ec82802b1
Antonio <jenkin90@gmail.com>
2015-08-10 18:03:05 +0200

    moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get("/author",function(req,res){
 });
 
 router.param('quizId',quizController.load);
+router.param('commentId',commentController.load);
 
 router.get("/quizes",quizController.index);
 
@@ -32,6 +33,7 @@ router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired,quizContro
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',sessionController.loginRequired,commentController.publish);
 
 router.get('/login',sessionController.new);
 router.post('/login',sessionController.create);
